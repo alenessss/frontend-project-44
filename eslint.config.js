8ceba783d9245053eb228c3df8e20001f65e6239
alena <aseremetevo@gmail.com>
2024-09-15 14:16:12 +0200

    efix slint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,7 @@ export default [
       ...importPlugin.configs.recommended.rules,
     },
   },
-  ...compat.extends('airbnb-base'),
+  ...compat.extends('standard-with-typescript'),
   {
     rules: {
       'no-underscore-dangle': [
