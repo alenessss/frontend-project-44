75401948638314df7392791cefa66b564d2e479d
alena <aseremetevo@gmail.com>
2024-10-01 23:33:51 +0200

    end fix cals

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,21 +1,42 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import СreatMainArchitect, { name, getRandomNumber } from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-console.log('What is the result of the expression?');
-
-// Выдает рандомное число
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min + 1;
+СreatMainArchitect();
 
 // Выдает случайный знак
-const GetRandomSign = (sign) => {
-  sign = ['+', '-', '*'];
+const GetRandomSign = () => {
+  const sign = ['+', '-', '*'];
   const i = sign[Math.floor(Math.random() * sign.length)];
-  console.log(i);
+  return i;
 };
 
-GetRandomSign();
+const FindTheAnswer = () => {
+  for (let step = 0; step < 3; step += 1) {
+    const NumberOne = getRandomNumber(1, 100);
+    const NumberTwo = getRandomNumber(1, 100);
+    const Symbol = GetRandomSign(3);
+    console.log(`Question: ${NumberOne} ${Symbol} ${NumberTwo}`);
+
+    let sum = 0;
+    if (Symbol === '*') {
+      sum = NumberOne * NumberTwo;
+    } else if (Symbol === '+') {
+      sum = NumberOne + NumberTwo;
+    } else if (Symbol === '-') {
+      sum = NumberOne - NumberTwo;
+    }
+    const answer = readlineSync.question('Your answer: ');
+    if (Number(answer) === sum) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${sum}.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+    if (step === 2) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
+FindTheAnswer();
