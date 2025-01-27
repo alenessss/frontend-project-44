d6fa20fb1c0d4e257c4f9e5708cfe5fb4a14fef0
Шереметьева <aseremetevo@gmail.com>
2025-01-27 18:26:23 +0200

    Изменение рандома символа

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,11 +1,7 @@
 import { runGame } from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
-export const getRandomSign = () => {
-  const sign = ['+', '-', '*'];
-  const i = sign[Math.floor(Math.random() * sign.length)];
-  return i;
-};
+const sign = ['+', '-', '*'];
 
 const calculate = (numberOne, numberTwo, symbol) => {
   if (symbol === '*') {
@@ -20,10 +16,10 @@ const calculate = (numberOne, numberTwo, symbol) => {
   return null;
 };
 
-const findTheAnswer = () => {
+const formingResponse = () => {
   const numberOne = getRandomNumber(1, 100);
   const numberTwo = getRandomNumber(1, 100);
-  const symbol = getRandomSign(3);
+  const symbol = sign[( getRandomNumber(-1, sign.length-1))];;
   const question = `${numberOne} ${symbol} ${numberTwo}`;
   const correctAnswer = String(calculate(numberOne, numberTwo, symbol));
   return [correctAnswer, question];
@@ -32,5 +28,5 @@ const findTheAnswer = () => {
 const exercise = 'What is the result of the expression?';
 
 export default () => {
-  runGame(findTheAnswer, exercise);
+  runGame(formingResponse, exercise);
 };
