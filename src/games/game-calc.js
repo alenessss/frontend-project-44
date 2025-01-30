bdeddb100ca31da2f3aa555e9fb2f790a44a83cb
Шереметьева <aseremetevo@gmail.com>
2025-01-30 19:40:18 +0200

    Изменение в линтера

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -19,7 +19,7 @@ const calculate = (numberOne, numberTwo, symbol) => {
 const formingResponse = () => {
   const numberOne = getRandomNumber(1, 100);
   const numberTwo = getRandomNumber(1, 100);
-  const symbol = sign[( getRandomNumber(-1, sign.length-1))];;
+  const symbol = sign[getRandomNumber(-1, sign.length - 1)];
   const question = `${numberOne} ${symbol} ${numberTwo}`;
   const correctAnswer = String(calculate(numberOne, numberTwo, symbol));
   return [correctAnswer, question];
